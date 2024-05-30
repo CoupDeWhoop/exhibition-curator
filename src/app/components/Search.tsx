515039c3b22221620c1d901285ab2af001703d29
CoupDeWhoop <greenland.david@gmail.com>
2024-05-30 15:33:23 +0100

    add search box

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push("/results/${}");
+    setSearch("");
+  };
+
+  return (
+    <form
+      className="flex justify-center md:justify-between"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search"
+        className="bg-white py-2 px-3 w-[260px] sm:w-80 text-xl rounded-xl text-black"
+      />
+    </form>
+  );
+}
